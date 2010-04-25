97ff6bff74407a5baddd1ea7d235fbbf97695d91
desmaj <matthew.desmarais@gmail.com>
2010-04-25 03:44:28 -0400

    forgot to add some stuff - better now

---
+++ hookbox/tests/__init__.py
@@ -0,0 +1,12 @@
+# load all modules to get them into the coverage report
+import hookbox
+from hookbox import admin
+from hookbox import channel
+from hookbox import config
+from hookbox import errors
+from hookbox import protocol
+from hookbox import rest
+from hookbox import server
+from hookbox import start
+from hookbox import user
+
