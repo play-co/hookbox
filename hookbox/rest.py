e66269e8426780784755fb17fd9079301c12ef9e
mcarter <cartermichael@gmail.com>
2010-05-05 17:25:56 -0700

    add subscribe to rest api

--- hookbox/rest.py
+++ hookbox/rest.py
@@ -34,7 +34,7 @@ class HookboxRest(object):
             self.logger.warn('REST Error: %s', path, exc_info=True)
             start_response('500 Internal server error', [])
             return json.dumps([False, {'msg': str(e) }])
-    
+        
     def render_publish(self, form, start_response):
         channel_name = form.get('channel_name', None)
         if not channel_name:
@@ -63,6 +63,24 @@ class HookboxRest(object):
         channel.unsubscribe(user, needs_auth=False)
         start_response('200 Ok', [])
         return json.dumps([True, {}])
+
+    def render_subscribe(self, form, start_response):
+        channel_name = form.get('channel_name', None)
+        if not channel_name:
+            raise Exception("Missing channel_name")
+        name= form.get('name', None)
+        if not name:
+            raise Exception("Missing name")
+        if not self.server.exists_channel(channel_name):
+            raise Exception("Channel %s doesn't exist" % (channel_name,))
+        if not self.server.exists_user(name):
+            raise Exception("User %s doesn't exist" % (name,))
+        channel = self.server.get_channel(None, channel_name)
+        user = self.server.get_user(name)
+        channel.subscribe(user, needs_auth=False)
+        start_response('200 Ok', [])
+        return json.dumps([True, {}])
+
 
 
     def render_disconnect(self, form, start_response):
