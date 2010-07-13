2f528ad9a15e46e789ceef071166221b57e8ddee
Martin Hunt <martin@mgh4.local>
2010-07-13 00:05:21 -0700

    fix unique id generation for automatic names in php_chat

--- examples/php_chat/public_html/connect.php
+++ examples/php_chat/public_html/connect.php
@@ -2,14 +2,12 @@
 
 session_start();
 
-
 if (empty($_SESSION['username']))
-    $_SESSION['username'] =  substr(uniqid('guest'),0,8);
+	$_SESSION['username'] =  'guest'.substr(base64_encode(rand(1000000000,9999999999)),0,3);
 
 $return = array();
 $return[0] = true;
 $return[1]['name'] = $_SESSION['username'];
-
 
 print_r(json_encode($return));
 
