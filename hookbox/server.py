ddef20da5df9582b3ea38449b6b42dbbece3baa6
Jannis Leidel <jannis@leidel.info>
2010-11-10 00:25:12 +0100

    Added --cbtrailingslash option that appends a slash to the callback URL. Useful for cases in which the target framework enforces trailing slashes with a redirect.

--- hookbox/server.py
+++ hookbox/server.py
@@ -162,6 +162,8 @@ class HookboxServer(object):
             host = u.hostname
             port = u.port or 80
             path = u.path
+            if self.config['cbtrailingslash'] and not path.endswith('/'):
+                path += '/'
             if u.query:
                 path += '?' + u.query
         else:
@@ -170,6 +172,8 @@ class HookboxServer(object):
 #                host = self.base_host
 #            else:
             path = self.base_path + '/' + self.config.get('cb_' + path_name)
+            if self.config['cbtrailingslash'] and not path.endswith('/'):
+                path += '/'
             scheme = self.config["cbhttps"] and "https" or "http"
             host = self.config["cbhost"]
             port = self.config["cbport"]
